2ab436fb879e158c6e1fdd89174ec3f0bb3f58ca
Megan Field <megan@sliips.com>
2018-08-14 09:48:21 +0100

    Basic file structure, bootstrap and animate.css implemented

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Navbar from './Navbar.js';
+
+import '../views/styles/home.css';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div className="home-background">
+				<Navbar />
+				<div className="container">
+				<p className="animated bounce delay-2s">jhksdhsjh</p>
+				</div>
+			</div>
+			)
+	}
+}
+
+export default Home;
